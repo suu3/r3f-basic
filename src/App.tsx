ef67f605f3483767ecb1f5165116110664d64edd
suu3 <dev.suu3@gmail.com>
2024-07-02 22:42:21 +0900

    ~18강

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import ThreeElement from "./ThreeElement";
+import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
 
 function App() {
+  const color = useControls({
+    value: "green",
+  });
+
+  const grid = useControls({
+    segment: { value: 10, min: 2, max: 100, step: 1 },
+  });
+
   return (
     <>
-      <Canvas>
+      <Canvas
+        camera={{
+          fov: 75,
+          near: 1,
+          far: 100,
+          position: [3, 3, 0],
+        }}
+      >
+        <color attach="background" args={[color.value]} />
+        <OrbitControls />
+        <axesHelper args={[6]} />
+        <gridHelper args={[10, grid.segment]} />
         <ThreeElement />
       </Canvas>
     </>
--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,16 +1,24 @@
 import { useFrame, useThree } from "@react-three/fiber";
 import { useRef } from "react";
 import * as THREE from "three";
+import { useControls } from "leva";
 
 const ThreeElement = () => {
   const { size, gl, scene, camera } = useThree();
   const boxRef = useRef<THREE.Mesh>(null);
+  const box = useControls({
+    rotation: {
+      value: 0,
+      min: -360,
+      max: 360,
+      step: 1,
+    },
+  });
 
   useFrame((state, delta) => {
-    console.log("state : ", state);
-    console.log("delta : ", delta);
-
-    boxRef.current.rotation.x += delta;
+    // console.log("state : ", state);
+    // console.log("delta : ", delta);
+    // boxRef.current.rotation.x += delta;
     // boxRef.current.rotation.y -= 0.02;
     // boxRef.current.scale.z += 0.01;
   });
@@ -23,8 +31,8 @@ const ThreeElement = () => {
         rotation={[
           // radian 단위라 우리가 아는 Degree에서 변환 필요
           THREE.MathUtils.degToRad(45),
-          THREE.MathUtils.degToRad(45),
-          THREE.MathUtils.degToRad(45),
+          THREE.MathUtils.degToRad(box.rotation),
+          0,
         ]}
       >
         <boxGeometry />
