408822f15ee8d67d18a139436f97a2068d78fe4b
sukyungbyun <sk.byun@muzlive.com>
2024-07-03 13:27:13 +0900

    20강

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,8 @@ import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import ThreeElement from "./ThreeElement";
 import { OrbitControls } from "@react-three/drei";
-import { useControls } from "leva";
 
 function App() {
-  const color = useControls({
-    value: "green",
-  });
-
-  const grid = useControls({
-    segment: { value: 10, min: 2, max: 100, step: 1 },
-  });
 
   return (
     <>
@@ -20,13 +12,13 @@ function App() {
           fov: 75,
           near: 1,
           far: 100,
-          position: [3, 3, 0],
+          position: [0, 0, 5],
         }}
       >
-        <color attach="background" args={[color.value]} />
+        <color attach="background" />
         <OrbitControls />
         <axesHelper args={[6]} />
-        <gridHelper args={[10, grid.segment]} />
+        <gridHelper args={[10,10]} />
         <ThreeElement />
       </Canvas>
     </>
--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,24 +1,15 @@
 import { useFrame, useThree } from "@react-three/fiber";
 import { useRef } from "react";
 import * as THREE from "three";
-import { useControls } from "leva";
 
 const ThreeElement = () => {
-  const { size, gl, scene, camera } = useThree();
   const boxRef = useRef<THREE.Mesh>(null);
-  const box = useControls({
-    rotation: {
-      value: 0,
-      min: -360,
-      max: 360,
-      step: 1,
-    },
-  });
 
   useFrame((state, delta) => {
+    
     // console.log("state : ", state);
     // console.log("delta : ", delta);
-    // boxRef.current.rotation.x += delta;
+    boxRef.current.position.x += 0.01;
     // boxRef.current.rotation.y -= 0.02;
     // boxRef.current.scale.z += 0.01;
   });
@@ -28,12 +19,9 @@ const ThreeElement = () => {
       <directionalLight position={[5, 5, 5]} />
       <mesh
         ref={boxRef}
-        rotation={[
-          // radian 단위라 우리가 아는 Degree에서 변환 필요
-          THREE.MathUtils.degToRad(45),
-          THREE.MathUtils.degToRad(box.rotation),
-          0,
-        ]}
+        position={[0,0,0]}
+        scale={[1,1,1]}
+        rotation={[THREE.MathUtils.degToRad(0),THREE.MathUtils.degToRad(0), THREE.MathUtils.degToRad(0)]}
       >
         <boxGeometry />
         <meshStandardMaterial color="red" />
