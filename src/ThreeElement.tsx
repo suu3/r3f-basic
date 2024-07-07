70a93e72c2ae1a0e1e124ff392bb649a90a27068
suu3 <dev.suu3@gmail.com>
2024-07-07 23:48:43 +0900

    26강

--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,30 +1,75 @@
-import { Box } from "@react-three/drei";
-import { useFrame, useThree } from "@react-three/fiber";
-import { useRef } from "react";
+// import { Box } from "@react-three/drei";
+// import { useFrame, useThree } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { useControls } from "leva";
 
 const ThreeElement = () => {
   const boxRef = useRef<THREE.Mesh>(null);
-
-  useFrame((state, delta) => {
-    // state.scene.rotation.x = THREE.MathUtils.degToRad(45);
+  const boxCopyRef = useRef<THREE.Mesh>(null);
+  const boxContorl = useControls({
+    width: {
+      value: 1,
+      min: 0.1,
+      max: 10,
+      step: 0.1,
+    },
+    height: {
+      value: 1,
+      min: 0.1,
+      max: 10,
+      step: 0.1,
+    },
+    depth: {
+      value: 1,
+      min: 0.1,
+      max: 10,
+      step: 0.1,
+    },
+    widthSeg: {
+      value: 1,
+      min: 0.1,
+      max: 10,
+      step: 0.1,
+    },
+    heightSeg: {
+      value: 1,
+      min: 0.1,
+      max: 10,
+      step: 0.1,
+    },
+    depthSeg: {
+      value: 1,
+      min: 0.1,
+      max: 10,
+      step: 0.1,
+    },
   });
+  // useFrame((state, delta) => {
+  //   // state.scene.rotation.x = THREE.MathUtils.degToRad(45);
+  // });
+
+  useEffect(() => {
+    boxCopyRef.current.geometry = boxRef.current?.geometry;
+  }, [boxContorl]);
 
   return (
     <>
       <directionalLight position={[5, 5, 5]} />
-      {/* geometry 만들기 */}
-      {/* 1. */}
-      <Box position={[-2, 0, 0]}>
-        <meshStandardMaterial color="green" />
-      </Box>
-      {/* 2. */}
-      <mesh geometry={new THREE.BoxGeometry(1, 1, 1)}>
-        <meshStandardMaterial color="blue" />
-      </mesh>
-      {/* 3. */}
       <mesh ref={boxRef} position={[0, 0, 0]}>
-        <boxGeometry />
+        <boxGeometry
+          args={[
+            boxContorl.width,
+            boxContorl.height,
+            boxContorl.depth,
+            boxContorl.widthSeg,
+            boxContorl.heightSeg,
+            boxContorl.depthSeg,
+          ]}
+        />
+        <meshStandardMaterial wireframe />
+      </mesh>
+      <mesh ref={boxCopyRef} position={[0, 0, 0]}>
         <meshStandardMaterial color="red" />
       </mesh>
     </>
