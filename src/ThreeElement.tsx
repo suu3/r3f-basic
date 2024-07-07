7df7ad3c4aaa324659af12e31536bfb5a7ed7c3d
suu3 <dev.suu3@gmail.com>
2024-07-07 23:22:04 +0900

    22강

--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -6,26 +6,56 @@ const ThreeElement = () => {
   const boxRef = useRef<THREE.Mesh>(null);
 
   useFrame((state, delta) => {
-    
-    // console.log("state : ", state);
-    // console.log("delta : ", delta);
-    boxRef.current.position.x += 0.01;
-    // boxRef.current.rotation.y -= 0.02;
-    // boxRef.current.scale.z += 0.01;
+    state.scene.rotation.x = THREE.MathUtils.degToRad(45);
   });
 
   return (
     <>
       <directionalLight position={[5, 5, 5]} />
-      <mesh
-        ref={boxRef}
-        position={[0,0,0]}
-        scale={[1,1,1]}
-        rotation={[THREE.MathUtils.degToRad(0),THREE.MathUtils.degToRad(0), THREE.MathUtils.degToRad(0)]}
-      >
-        <boxGeometry />
-        <meshStandardMaterial color="red" />
-      </mesh>
+
+      <group position={[0, 0, 3]}>
+        <axesHelper args={[5]} />
+        <mesh
+          ref={boxRef}
+          position={[0, 0, 0]}
+          scale={[1, 1, 1]}
+          rotation={[
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+          ]}
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="red" />
+        </mesh>
+        <mesh
+          ref={boxRef}
+          position={[0, 2, 0]}
+          scale={[1, 1, 1]}
+          rotation={[
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+          ]}
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="green" />
+        </mesh>
+        <mesh
+          ref={boxRef}
+          position={[2, 0, 0]}
+          scale={[1, 1, 1]}
+          rotation={[
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+          ]}
+        >
+          <axesHelper args={[3]} />
+          <boxGeometry />
+          <meshStandardMaterial color="blue" />
+        </mesh>
+      </group>
     </>
   );
 };
