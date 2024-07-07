712c8f8c9bc14aa4f94dfc7281fc00cf665402e5
suu3 <dev.suu3@gmail.com>
2024-07-07 23:31:14 +0900

    25강

--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { useRef } from "react";
 import * as THREE from "three";
@@ -6,56 +7,26 @@ const ThreeElement = () => {
   const boxRef = useRef<THREE.Mesh>(null);
 
   useFrame((state, delta) => {
-    state.scene.rotation.x = THREE.MathUtils.degToRad(45);
+    // state.scene.rotation.x = THREE.MathUtils.degToRad(45);
   });
 
   return (
     <>
       <directionalLight position={[5, 5, 5]} />
-
-      <group position={[0, 0, 3]}>
-        <axesHelper args={[5]} />
-        <mesh
-          ref={boxRef}
-          position={[0, 0, 0]}
-          scale={[1, 1, 1]}
-          rotation={[
-            THREE.MathUtils.degToRad(0),
-            THREE.MathUtils.degToRad(0),
-            THREE.MathUtils.degToRad(0),
-          ]}
-        >
-          <boxGeometry />
-          <meshStandardMaterial color="red" />
-        </mesh>
-        <mesh
-          ref={boxRef}
-          position={[0, 2, 0]}
-          scale={[1, 1, 1]}
-          rotation={[
-            THREE.MathUtils.degToRad(0),
-            THREE.MathUtils.degToRad(0),
-            THREE.MathUtils.degToRad(0),
-          ]}
-        >
-          <boxGeometry />
-          <meshStandardMaterial color="green" />
-        </mesh>
-        <mesh
-          ref={boxRef}
-          position={[2, 0, 0]}
-          scale={[1, 1, 1]}
-          rotation={[
-            THREE.MathUtils.degToRad(0),
-            THREE.MathUtils.degToRad(0),
-            THREE.MathUtils.degToRad(0),
-          ]}
-        >
-          <axesHelper args={[3]} />
-          <boxGeometry />
-          <meshStandardMaterial color="blue" />
-        </mesh>
-      </group>
+      {/* geometry 만들기 */}
+      {/* 1. */}
+      <Box position={[-2, 0, 0]}>
+        <meshStandardMaterial color="green" />
+      </Box>
+      {/* 2. */}
+      <mesh geometry={new THREE.BoxGeometry(1, 1, 1)}>
+        <meshStandardMaterial color="blue" />
+      </mesh>
+      {/* 3. */}
+      <mesh ref={boxRef} position={[0, 0, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="red" />
+      </mesh>
     </>
   );
 };
