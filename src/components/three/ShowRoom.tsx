a7030fd63cd2b092f4d95c50622719efc0c4eb54
suu3 <dev.suu3@gmail.com>
2024-07-14 04:33:15 +0900

    52강

--- src/components/three/ShowRoom.tsx
+++ src/components/three/ShowRoom.tsx
@@ -1,9 +1,9 @@
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import * as THREE from "three";
 import { useThree } from "@react-three/fiber";
 import { CameraControls } from "@react-three/drei";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export default function ShowRoom() {
   const gltf = useLoader(GLTFLoader, "/models/custom.glb");
@@ -12,6 +12,33 @@ export default function ShowRoom() {
 
   console.log("gltf : ", gltf);
   const cameraControlsRef = useRef<CameraControls>(null);
+
+  window.addEventListener("keydown", (e) => {
+    console.log("e.key: ", e.key);
+    switch (e.key) {
+      case "a":
+        cameraControlsRef.current?.setLookAt(-2, 0, 2, 0, 0, 0, true);
+        break;
+      case "b":
+        cameraControlsRef.current?.setLookAt(0, 3, 0, 0, 0, 0, true);
+        break;
+    }
+  });
+
+  useEffect(() => {
+    cameraControlsRef.current?.setTarget(0, 0, 0);
+  });
+
+  let angle = 0;
+  let dis = 2; //distance. 클수록 멀리
+  useFrame(() => {
+    cameraControlsRef.current?.setPosition(
+      dis * Math.sin(angle),
+      0.8, //살짝 위에서 움직임
+      dis * Math.sin(angle)
+    );
+    angle = angle + 0.01; //속도
+  });
 
   const shoesClick = () => {
     const intersects = raycaster.intersectObjects(gltf.scene.children, true);
