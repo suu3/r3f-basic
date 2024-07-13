0dde367c75ca05f2e314119c9c03cd67bb0a7a63
suu3 <dev.suu3@gmail.com>
2024-07-14 01:02:15 +0900

    47강

--- src/components/three/ShowRoom.tsx
+++ src/components/three/ShowRoom.tsx
@@ -1,19 +1,35 @@
-import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { useLoader } from "@react-three/fiber";
-import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import * as THREE from "three";
+import { useThree } from "@react-three/fiber";
 
 export default function ShowRoom() {
-  // const obj = useLoader(OBJLoader, "/models/custom.obj");
-  // const fbx = useLoader(FBXLoader, "/models/custom.fbx");
   const gltf = useLoader(GLTFLoader, "/models/custom.glb");
+
+  const { raycaster } = useThree();
+
+  console.log("gltf : ", gltf);
+
+  const shoesClick = () => {
+    const intersects = raycaster.intersectObjects(gltf.scene.children, true);
+    console.log("intersects : ", intersects);
+
+    if (intersects.length > 0) {
+      const firstObj = intersects[0].object as THREE.Mesh;
+      const firstMat = firstObj.material as THREE.MeshStandardMaterial;
+      const cloneMat = firstMat.clone();
+
+      firstObj.material = cloneMat;
+      const mat = firstObj.material as THREE.MeshStandardMaterial;
+      mat.color = new THREE.Color("red");
+    }
+  };
 
   return (
     <>
       {/* <primitive object={obj} />
       <primitive object={fbx} /> */}
-      <primitive object={gltf.scene} />
+      <primitive object={gltf.scene} onClick={shoesClick} />
       {/* <mesh
         rotation={[
           THREE.MathUtils.degToRad(45),
