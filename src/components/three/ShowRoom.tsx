50d4b159e825df3310e25de1c72363dfb39d001c
suu3 <dev.suu3@gmail.com>
2024-07-14 00:43:57 +0900

    46강

--- src/components/three/ShowRoom.tsx
+++ src/components/three/ShowRoom.tsx
@@ -1,12 +1,29 @@
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { useLoader } from "@react-three/fiber";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import * as THREE from "three";
 
 export default function ShowRoom() {
+  // const obj = useLoader(OBJLoader, "/models/custom.obj");
+  // const fbx = useLoader(FBXLoader, "/models/custom.fbx");
+  const gltf = useLoader(GLTFLoader, "/models/custom.glb");
+
   return (
-    <mesh
-      rotation={[THREE.MathUtils.degToRad(45), THREE.MathUtils.degToRad(45), 0]}
-    >
-      <boxGeometry />
-      <meshStandardMaterial />
-    </mesh>
+    <>
+      {/* <primitive object={obj} />
+      <primitive object={fbx} /> */}
+      <primitive object={gltf.scene} />
+      {/* <mesh
+        rotation={[
+          THREE.MathUtils.degToRad(45),
+          THREE.MathUtils.degToRad(45),
+          0,
+        ]}
+      >
+        <boxGeometry />
+        <meshStandardMaterial />
+      </mesh> */}
+    </>
   );
 }
